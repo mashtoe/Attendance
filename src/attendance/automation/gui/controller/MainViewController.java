d6946db4a7803f88d661c5f8a5d34136ea1a4bd5
Stefan O <input.goes.here@gmail.com>
2017-02-09 12:49:57 +0100

    Revert "GUI"
    This reverts commit 0b1654cc3bb1c0de8351d9aa6dc78460e0c05b93.

--- src/attendance/automation/gui/controller/MainViewController.java
+++ src/attendance/automation/gui/controller/MainViewController.java
@@ -5,15 +5,10 @@
  */
 package attendance.automation.gui.controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -31,19 +26,7 @@ public class MainViewController implements Initializable
     @FXML
     private HBox hBox;
     @FXML
-    private VBox vBoxTimestamp;
-    @FXML
     private VBox vBoxMonday;
-    @FXML
-    private VBox vBoxTuesday;
-    @FXML
-    private VBox vBoxWednesday;
-    @FXML
-    private VBox vBoxThursday;
-    @FXML
-    private VBox vBoxFriday;
-
-
 
     /**
      * Initializes the controller class.
@@ -51,28 +34,7 @@ public class MainViewController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        try
-        {
-            vBoxMonday.getChildren().add(getClassModul());
-            vBoxMonday.getChildren().add(getClassModul());
-            vBoxMonday.getChildren().add(getClassModul());
-        } catch (IOException ex)
-        {
-            Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }  
-    
-    
-
-    private Node getClassModul() throws IOException
-    {
-        
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/attendance/automation/gui/view/Class.fxml"));
-        Node node = loader.load();
-       // ClassController controller = loader.getController();
-        return node;
-        
-        
-    }
+       
+    }    
     
 }
