7c4a62957f4af47e426931ed81f90e0fce9fab2d
mashtoe <mashtoe@hotmail.com>
2017-02-09 11:45:24 +0100

    gitignore file fix maybe

--- src/attendance/automation/dal/AttendanceDAO.java
+++ src/attendance/automation/dal/AttendanceDAO.java
@@ -11,5 +11,21 @@ package attendance.automation.dal;
  */
 public class AttendanceDAO
 {
+    int[][] schedule = new int[5][8];
     
+    public int[][] getSchedule(String... fag)
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            int[] day = new int[8];
+            
+            for (int j = 0; j < day.length; j++)
+            {
+                
+            }
+            
+        }
+        return schedule;
+    }
+     
 }
