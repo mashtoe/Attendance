c6f99faf277833b8f3ec256b77fa8f53c9b9e345
Noxgene <datnoxgene@gmail.com>
2017-02-09 11:07:53 +0100

    Student setup in the BE package.

--- src/attendance/automation/be/Student.java
+++ src/attendance/automation/be/Student.java
@@ -11,5 +11,74 @@ package attendance.automation.be;
  */
 public class Student
 {
+    private String name;
+    
+    private String schoolclass;
+    
+    private double attendance;
+    
+    private String email;
+    
+    private int phonenum;
+
+    public Student(String name, String schoolclass, double attendance, String email, int phonenum)
+    {
+        this.name = name;
+        this.schoolclass = schoolclass;
+        this.attendance = attendance;
+        this.email = email;
+        this.phonenum = phonenum;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getSchoolclass()
+    {
+        return schoolclass;
+    }
+
+    public void setSchoolclass(String schoolclass)
+    {
+        this.schoolclass = schoolclass;
+    }
+
+    public double getAttendance()
+    {
+        return attendance;
+    }
+
+    public void setAttendance(double attendance)
+    {
+        this.attendance = attendance;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public int getPhonenum()
+    {
+        return phonenum;
+    }
+
+    public void setPhonenum(int phonenum)
+    {
+        this.phonenum = phonenum;
+    }
+    
     
 }
